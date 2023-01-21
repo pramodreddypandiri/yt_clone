3528706b783978c04dfcf6bdcc1b4f235371b0f7
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2023-01-21 21:47:01 +0530

    search functionality done

--- src/components/ChannelDetails.jsx
+++ src/components/ChannelDetails.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
-
-const ChannelDetails = () => {
+import {useState, useEffect} from "react"
+import { useParams } from 'react-router-dom'
+import {Box} from '@mui/material'
+import {Videos, ChannelCard} from '../components'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+const ChannelDetails = () =>{
+  const {id} = useParams()
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+  useEffect(() => {
+      //fetch channel details
+      fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]))
+      // fetch videos of the channel
+      fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) => setVideos(data?.items))
+  },[id])
   return (
-    <div>ChannelDetails</div>
+      <Box minHeight='95vh'>
+        <Box>
+          <div style={{background: ' linear-gradient(90deg, rgba(0,217,255,1) 36%, rgba(9,116,121,1) 85%)', zIndex: 10, height:'300px'}} />
+          <ChannelCard channelDetail={channelDetail} marginTop='-110px'/>
+        </Box>
+        <Box display='flex' p='2'>
+           <Box sx={{mr:{sm:'100px'}}}/>
+           <Videos videos={videos} />
+        </Box>
+        
+      </Box>
+      
+    
+    
   )
-}
+ }
 
 export default ChannelDetails
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
-// this comp appears when searched 
-const SearchFeed = () => {
+import { useState, useEffect } from 'react'
+import { Box, Typography } from '@mui/material'
+import { Videos} from '../components'
+import {useParams} from 'react-router-dom'
+
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+const Feed = () => {
+  const {searchTerm} = useParams()
+  const [videos, setVideos] = useState([])
+  
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => setVideos(data?.items))
+  },
+    [searchTerm])
+  //console.log(selectedCategory);
+  //console.log(videos);
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{overflowY: 'auto', height: '90vh',flex:2}}>
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{color:'white'}}>Search reslts for: 
+          <span style={{color: '#F31503'}}> {searchTerm}</span>Videos
+        </Typography>
+        {/* send videos(got from api) to Videos comp */}
+        <Videos videos={videos}/>
+      </Box>
+
   )
 }
 
-export default SearchFeed
+export default Feed
